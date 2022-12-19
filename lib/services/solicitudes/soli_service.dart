1bc9a633750d60e698b69a077214d0b9b9aa2ef7
jesm-erick <jhonsucasaire@upeu.edu.pe>
2022-12-19 15:26:16 -0500

    finalizacion de service

---
+++ lib/services/solicitudes/soli_service.dart
@@ -0,0 +1,68 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:proyecto_integrador_c6/services/solicitudes/soli.dart';
+import 'package:http/http.dart' as http;
+
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:proyecto_integrador_c6/services/solicitudes/solis.dart';
+
+class SolicitudDBService {
+  static final String API_URL = dotenv.get('API_HOST');
+
+  static Future<List<Solicitud>> getSolicitudes() async {
+    var url = API_URL + "/apiv1/solicitud/all";
+    final respuesta = await http.get(Uri.parse(url));
+
+    if (respuesta.statusCode == 200) {
+      final respuestaJSON = jsonDecode(respuesta.body);
+      final listaPeliculas = Solicitudes.fromJsonList(respuestaJSON["data"]);
+      return listaPeliculas;
+    }
+    return <Solicitud>[];
+  }
+
+  static Future<int> createSolicitud(
+      String id, Map<String, dynamic> data) async {
+    var url = API_URL + "/apiv1/solicitud/" + id;
+    final respuesta = await http.post(Uri.parse(url),
+        headers: {
+          HttpHeaders.contentTypeHeader: "application/json",
+        },
+        body: jsonEncode(data));
+    if (respuesta.statusCode == 200) {
+      final respuestaJSON = jsonDecode(respuesta.body);
+      // final listaPeliculas = Solicitudes.fromJsonList(respuestaJSON["data"]);
+      return 1;
+    }
+    return 0;
+  }
+
+  static Future<int> updateSolicitud(
+      String id, Map<String, dynamic> data) async {
+    //print(data);
+    var url = API_URL + "/apiv1/solicitud/" + id;
+    final respuesta = await http.put(Uri.parse(url),
+        headers: {HttpHeaders.contentTypeHeader: "application/json"},
+        body: jsonEncode(data));
+    if (respuesta.statusCode == 200) {
+      //print(respuesta.body);
+      final respuestaJSON = jsonDecode(respuesta.body);
+      return respuestaJSON["data"][0];
+    }
+    return 0;
+  }
+
+  static Future<int> deleteSolicitud(String id) async {
+    var url = API_URL + "/apiv1/solicitud/" + id;
+    final respuesta = await http.delete(
+      Uri.parse(url),
+      headers: {HttpHeaders.contentTypeHeader: "application/json"},
+    );
+    if (respuesta.statusCode == 200) {
+      final respuestaJSON = jsonDecode(respuesta.body);
+      return respuestaJSON["data"];
+    }
+    return 0;
+  }
+}
---
+++ lib/services/solicitudes/solis.dart
@@ -0,0 +1,15 @@
+import 'package:proyecto_integrador_c6/services/solicitudes/soli.dart';
+
+class Solicitudes {
+  Solicitudes();
+  static List<Solicitud> fromJsonList(List<dynamic> jsonList) {
+    List<Solicitud> listaSolicitudes = [];
+    if (jsonList != null) {
+      for (var solicitud in jsonList) {
+        final soli = Solicitud.fromJson(solicitud);
+        listaSolicitudes.add(soli);
+      }
+    }
+    return listaSolicitudes;
+  }
+}
