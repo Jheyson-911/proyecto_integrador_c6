a13c3695266f1ca6c687e4db38282b6b83aee869
21Maryori <cosimaryo@gmail.com>
2022-12-21 13:10:00 -0500

    Informacion2.0

--- lib/ui/components/informacion/informacion_page.dart
+++ lib/ui/components/informacion/informacion_page.dart
@@ -6,125 +6,266 @@ class InformacionPage extends StatefulWidget {
   @override
   State<InformacionPage> createState() => _InformacionPageState();
 }
+ _launchURL() async {
+   print("HOLA");
+  }
+
+  Widget _ButtomWTSP() {
+    return FloatingActionButton(
+      onPressed: _launchURL,
+      child: GestureDetector(
+        child: Icon(Icons.whatsapp),
+      ),
+    );
+  }
+
 
 class _InformacionPageState extends State<InformacionPage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(20),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const Text(
-            "A partir del 6to ciclo académico y los créditos aprobados, ya puedes realizar tus practicas pre profesionales en una organización publica, privada o mixta donde desarrolles actividades afines a la carrera. \nLos alumnos que no cuenten con el 6to ciclo y los créditos aprobados, no podrá realizar sus practicas pre profesionales",
-            style: TextStyle(fontSize: 18, fontWeight: FontWeight.w400),
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-          const Text(
-            "Finalidad",
-            style: TextStyle(fontSize: 23, fontWeight: FontWeight.bold),
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-          Row(
-            children: const [
-              SizedBox(
-                width: 15,
-              ),
-              Text(
-                "• ",
-              ),
-              Text(
-                  "Complementar y afirmar los conocimientos impartidos en la\nuniversidad con la experiencia en la actividad profesional.")
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Scaffold(
+          floatingActionButton: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: _ButtomWTSP(),
+              )
             ],
           ),
-          Row(
-            children: const [
-              SizedBox(
-                width: 15,
-              ),
-              Text(
-                "• ",
-              ),
-              Text(
-                  "Lograr una adecuada adaptación del estudiante a la actividad\nprofesional.")
-            ],
+          body: SingleChildScrollView(
+            child: Column(
+              children: [
+                Container(
+                    margin: const EdgeInsets.only(bottom: 0),
+                    child: Image.network(
+                      'https://lh3.googleusercontent.com/-hWH-rEdRNFE/WB9i2aaHFuI/AAAAAAAAI-M/w3yNtA7Bjvs/s640/blogger-image--575121095.jpg',
+                      height: 300,
+                      width: 800,
+                      fit: BoxFit.contain,
+                    )),
+                Column(
+                  children: [
+                    Container(
+                      padding: EdgeInsets.all(10),
+                      child: Text(
+                        '¿QUÉ SON LAS PRACTICAS PRE PROFESIONALES?',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 20),
+                      ),
+                    ),
+
+                    Container(
+                      padding: EdgeInsets.only(left: 20, right: 20, bottom: 30),
+                      child: Text(
+                        'Es la modalidad que permite a la persona en formación durante su condición de estudiante aplicar sus conocimientos, habilidades y aptitudes mediante el desempeño en una situación real de trabajo.',
+                        textAlign: TextAlign.justify,
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 15),
+                      ),
+                    ),
+
+
+                    Container(
+                    margin: const EdgeInsets.only(bottom: 0),
+                    child: Image.network(
+                      'https://upeu.edu.pe/wp-content/uploads/2022/06/Ceremonia-de-investidura-de-pra%CC%81cticas-pre-profesionales-a-25-estudiantes-de-Ingenieri%CC%81a-de-Sistemas-de-la-UPeU-Juliaca0.jpeg',
+                      height: 300,
+                      width: 800,
+                      fit: BoxFit.contain,
+                    )),
+
+
+                    Container(
+                      padding: EdgeInsets.all(10),
+                      child: Text(
+                        'REGLAMENTO DE PRACTICAS PRE PROFESIONALES DE LA ESCUELA DE INGENIERIA DE SISTEMAS ',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 20),
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.all(10),
+                      child: Text(
+                        'CAPITULO 1 DE LAS DISPOSICIONES GENERALES ',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 18),
+                      ),
+                    ),
+
+                    Container(
+                      padding: EdgeInsets.only(left: 20, right: 20, bottom: 30),
+                      child: Text(
+                        'ARTICULO 1: Las practicas Pre Profesionales son de caracter obligatorio y requisito para la obtencion del grado de Bachiller en la Escuela Profesional de Ingenieria de Sistemas. ',
+                        textAlign: TextAlign.justify,
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 17),
+                      ),
+                    ),
+
+                    Container(
+                      padding: EdgeInsets.only(left: 20, right: 20, bottom: 30),
+                      child: Text(
+                        'ARTICULO3: Las Practicas Pre Profesionales tendran una duracion nimina de 750 horas acumulables en una o varias actividades diferentes.',
+                        textAlign: TextAlign.justify,
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 17),
+                      ),
+                    ),
+
+                    Container(
+                      padding: EdgeInsets.only(left: 20, right: 20, bottom: 30),
+                      child: Text(
+                        'N° de horas en empresa = 700 + N° de horas de proyeccion social = 50',
+                
+                        textAlign: TextAlign.justify,
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 17),
+                      ),
+                      
+                    ),
+
+
+                    Container(
+                    margin: const EdgeInsets.only(bottom: 0),
+                    child: Image.network(
+                      'https://upeu.edu.pe/wp-content/uploads/2022/10/WhatsApp-Image-2022-10-18-at-9.02.09-AM-scaled.jpeg',
+                      height: 300,
+                      width: 800,
+                      fit: BoxFit.contain,
+                    )),
+
+
+                    Container(
+                      padding: EdgeInsets.only(left: 20, right: 20, bottom: 30),
+                      child: Text(
+                        '¿COMÓ INICIAR MIS PPP?',
+                
+                        textAlign: TextAlign.justify,
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 20),
+                      ),
+                      
+                    ),
+
+
+                    Container(
+                       padding: EdgeInsets.all(10),
+                      child: Text(
+                        '1. Solicita a la secretaria de la escuela la CARTA DE PRESENTACION (https://forms.gle/3mykY5TGopdQAMKo9).',
+                
+                        textAlign: TextAlign.justify,
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 15),
+                      ),
+                      
+                    ),
+
+                    Container(
+                       padding: EdgeInsets.all(10),
+                      child: Text(
+                        '2. Recepciona de la secretaría la carta de presentación y llevala al centro de prácticas Solicita del centro de practicas tu carta de aceptación',
+                
+                        textAlign: TextAlign.justify,
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 15),
+                      ),
+                      
+                    ),
+
+                    Container(
+                       padding: EdgeInsets.all(10),
+                      child: Text(
+                        '3. Solicita del centro de practicas tu carta de aceptación',
+                
+                        textAlign: TextAlign.justify,
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 15),
+                      ),
+                      
+                    ),
+
+                    Container(
+                       padding: EdgeInsets.all(10),
+                      child: Text(
+                        '4. Notifica al coordinador de PPP (Ing...), sobre el inicio de las prácticas y entrega el digital de los documentos',
+                
+                        textAlign: TextAlign.justify,
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 15),
+                      ),
+                      
+                    ),
+
+                    Container(
+                       padding: EdgeInsets.all(10),
+                      child: Text(
+                        '5. Una vez iniciado la prácticas, elabora tu plan de prácticas y gestiona tu convenio de prácticas',
+                
+                        textAlign: TextAlign.justify,
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 15),
+                      ),
+                      
+                    ),
+
+                    Container(
+                       padding: EdgeInsets.all(10),
+                      child: Text(
+                        '6. El coordinador de PPP cronograma según tu plan de prácticas 3 supervisiones(inicial, intermedia y final)',
+                
+                        textAlign: TextAlign.justify,
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 15),
+                      ),
+                    ),
+
+                    Container(
+                    margin: const EdgeInsets.only(bottom: 0),
+                    child: Image.network(
+                      'https://upeu.edu.pe/wp-content/uploads/2022/03/380-bachilleres-de-la-UPeU-Campus-Juliaca-participaron-en-la-emotiva-ceremonia-de-graduacio%CC%81n-20212-1080x675.jpeg',
+                      height: 300,
+                      width: 800,
+                      fit: BoxFit.contain,
+                    )),
+
+                    Container(
+                       padding: EdgeInsets.all(10),
+                      child: Text(
+                        '7.Al término de tus prácticas entrega al tu jefe inmediato la ficha de evaluación de prácticas',
+                
+                        textAlign: TextAlign.justify,
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 15),
+                      ),
+                      
+                    ),
+
+                    Container(
+                       padding: EdgeInsets.all(10),
+                      child: Text(
+                        '8.Todos estos documentos formarán parte de tu informe de prácticas pre profesionales el cual se sustentará frente a un jurado de docentes.',
+                        
+                        textAlign: TextAlign.justify,
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 15),
+                      ),
+                      
+                    ),
+
+
+                  ],
+                ),
+              ],
+            ),
           ),
-          Row(
-            children: const [
-              SizedBox(
-                width: 15,
-              ),
-              Text(
-                "• ",
-              ),
-              Text(
-                  "Lograr en el estudiante el desarrollo de habilidades, destrezas\ny sólida formación ética en el desempeño de sus funciones.")
-            ],
-          ),
-          // const SizedBox(
-          //   height: 10,
-          // ),
-          // const Text(
-          //     "Si tiene dudas o consultas, puede enviar un correo a oficinadepracticas@upeu.edu.pe"),
-          const SizedBox(
-            height: 10,
-          ),
-          const Text(
-            "Duración",
-            style: TextStyle(fontSize: 23, fontWeight: FontWeight.bold),
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-          Row(
-            children: const [
-              SizedBox(
-                width: 15,
-              ),
-              Text(
-                "• ",
-              ),
-              Text("Ingenierias: 700 horas (7 meses aprox.)")
-            ],
-          ),
-          Row(
-            children: const [
-              SizedBox(
-                width: 15,
-              ),
-              Text(
-                "• ",
-              ),
-              Text("Ciencias Empresariales: 700 horas (7 meses aprox.)")
-            ],
-          ),
-          Row(
-            children: const [
-              SizedBox(
-                width: 15,
-              ),
-              Text(
-                "• ",
-              ),
-              Text("Ciencias de la Salud: 700 horas (7 meses aprox.)")
-            ],
-          ),
-          Row(
-            children: const [
-              SizedBox(
-                width: 15,
-              ),
-              Text(
-                "• ",
-              ),
-              Text("Ciencias de la Educación: 700 horas (7 meses aprox.)")
-            ],
-          ),
-        ],
+        ),
       ),
     );
   }
